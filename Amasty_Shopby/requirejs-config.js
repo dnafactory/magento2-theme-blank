95407010e9e66f6eddfba50b9683bb718bfaeddd
Ciro Arcadio <ciro@dnafactory.it>
2021-04-20 09:54:03 +0200

    - aggiunta della selezione multipla sulla toolbar in Amasty_Shopby

---
+++ Amasty_Shopby/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins:{
+            "Amasty_Shopby/js/amShopbyAjax":{
+                "Amasty_Shopby/js/amShopbyAjax-url-history-fix-mixin": true
+            }
+        }
+    }
+};
