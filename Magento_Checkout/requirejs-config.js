f702eb3d209513060ffd96fcc4cb41caa2bd71f0
Ciro Arcadio <ciro@dnafactory.it>
2021-07-13 16:48:15 +0200

    Aggiunto JS component per gestire le quantità nel carrello

---
+++ Magento_Checkout/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins:{
+            "Magento_Checkout/js/shopping-cart":{
+                "Magento_Checkout/js/shopping-cart-mixin": true
+            }
+        }
+    }
+};
