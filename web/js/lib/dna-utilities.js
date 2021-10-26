6e3e8ea6553a521a57c844be0847da37f16f49bc
Ciro Arcadio <ciro@dnafactory.it>
2021-10-26 16:35:20 +0200

    + aggiunte options nell'helper di attach/remove Event Listeners in utilities

--- web/js/lib/dna-utilities.js
+++ web/js/lib/dna-utilities.js
@@ -136,18 +136,20 @@ define(['underscore'], function(_) {
          * @param element
          * @param events
          * @param handler
+         * @param options
          */
-        attachEvents(element, events, handler){
-            this.cycleItems(events, (event) => element.addEventListener(event, handler, false));
+        attachEvents(element, events, handler, options = false){
+            this.cycleItems(events, (event) => element.addEventListener(event, handler, options));
         },
         /**
          * Removes multiple event listeners from a single HTML element on the events given as a space separated string
          * @param element
          * @param events
          * @param handler
+         * @param options
          */
-        removeEvents(element, events, handler){
-            this.cycleItems(events, (event) => element.removeEventListener(event, handler, false));
+        removeEvents(element, events, handler, options = false){
+            this.cycleItems(events, (event) => element.removeEventListener(event, handler, options));
         },
         getCssValue(element, property, defaultValue = null){
             var value = this.nullishCoalescingValue(
